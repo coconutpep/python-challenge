be57f17f7a51f19a10a1c86e3fad9f11d067cc0f
unknown <ryanklueg@gmail.com>
2020-02-15 13:11:45 -0500

    Eigth commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,8 +4,13 @@ import os
 csvpath = os.path.join('..', 'PyPoll', 'election_data.csv')
 
 votesTotal = 0
+canidates = []
+canidateVotes = {}
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimitter=',')
     csv_header = next(csvreader)
     for row in csvreader:
         votesTotal += 1
+        if row[2] not in canidates:
+            canidates.append(row[2])
+        
--- README.md
+++ README.md
@@ -26,3 +26,6 @@
   # Seventh Commit
     # Fixed code in PyBank's main.py to calculate changes in profit/loss and related statistics rather than just statistics for the profit/loss
     # Replaiced finanical_analysis.txt with updated accurate example
+  
+  # Eigth Commit
+    # Added code to PyPoll's main.py file to find the list of canidates that recieved votes
