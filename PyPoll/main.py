e5805d4417a6ab8d8c0fa72e1d3954fe4bcfa66b
unknown <ryanklueg@gmail.com>
2020-02-15 13:52:27 -0500

    Ninth Commit: Canidate Dictionary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,10 +7,13 @@ votesTotal = 0
 canidates = []
 canidateVotes = {}
 with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimitter=',')
+    csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
     for row in csvreader:
         votesTotal += 1
         if row[2] not in canidates:
             canidates.append(row[2])
-        
+            print(canidates)
+        if row[2] not in canidateVotes:
+            canidateVotes[row[2]] = 1
+            print(canidateVotes)
