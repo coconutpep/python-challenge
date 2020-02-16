06f74137dca46b0692ddecffd09dce85cdb6fe97
unknown <ryanklueg@gmail.com>
2020-02-16 14:48:26 -0500

    Winner calculation and print to terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,8 +17,20 @@ with open(csvpath, newline='') as csvfile:
         elif row[2] in canidateVotes:
             canidateVotes[row[2]] += 1
 
+winnervalue = max(canidateVotes.values())
+winner = list(canidateVotes.keys())[list(canidateVotes.values()).index(winnervalue)]
+print(winner)
 canidatePercents = {}
 for canidate in canidateVotes:
     if canidateVotes[canidate] not in canidatePercents:
         canidatePercents[canidate] = round(((canidateVotes[canidate] / votesTotal) * 100),3)
 
+print("Election Results")
+print("-------------------------------")
+print("Total Votes: " + str(votesTotal))
+print("-------------------------------")
+for canidate in canidates:
+    print(str(canidate) + ": " + str(canidatePercents[canidate]) + "% " + "(" + str(canidateVotes[canidate]) + ")")
+print("-------------------------------")
+print("Winner: " + str(winner))
+print("-------------------------------")
