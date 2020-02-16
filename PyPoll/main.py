27f9400445c1b18a524c19848ce58a0818ff9690
unknown <ryanklueg@gmail.com>
2020-02-16 13:55:11 -0500

    Canidate Vote Tally fixed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,9 +13,6 @@ with open(csvpath, newline='') as csvfile:
         votesTotal += 1
         if row[2] not in canidates:
             canidates.append(row[2])
-        elif row[2] not in canidateVotes:
             canidateVotes[row[2]] = 1
         elif row[2] in canidateVotes:
             canidateVotes[row[2]] += 1
-
-
